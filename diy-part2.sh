dfb1c40058fa7b3e5299bb27604ee5c85a4994e8
yeungle <75871729+yeungle@users.noreply.github.com>
2025-06-19 10:36:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,11 +25,11 @@
 
 #git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
 
-#git clone --depth=1 https://github.com/kenzok8/small-package.git kenzok8-packages
-#cp -rf kenzok8-packages/ddnsto package/ddnsto
-#cp -rf kenzok8-packages/luci-app-ddnsto package/luci-app-ddnsto
+git clone --depth=1 https://github.com/kenzok8/small-package.git kenzok8-packages
+cp -rf kenzok8-packages/ddnsto package/ddnsto
+cp -rf kenzok8-packages/luci-app-ddnsto package/luci-app-ddnsto
 
 #git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
-#./scripts/feeds update -a
-#./scripts/feeds install -a
+./scripts/feeds update -a
+./scripts/feeds install -a
