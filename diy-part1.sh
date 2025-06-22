3769a770f15df5d4c9e5cc93a73abe2b2d583633
yeunglele <yangle@cecchina.com>
2025-06-22 10:27:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 sed -i '1i src-git moruiris https://github.com/moruiris/openwrt-packages;openwrt' feeds.conf.default
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
